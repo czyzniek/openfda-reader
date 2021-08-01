1b576fb160ba58d175028ed81f488ca49d4b9b2d
czyzniek <kamil.czyznielewski@gmail.com>
2021-08-01 12:32:59 +0200

    Fixing Pageable object in swagger Adding missing openfda field in openFDA response

--- src/main/java/pl/czyzniek/openfdareader/drugrecordapplication/fda/DrugRecordApplicationControllerDefinition.java
+++ src/main/java/pl/czyzniek/openfdareader/drugrecordapplication/fda/DrugRecordApplicationControllerDefinition.java
@@ -3,7 +3,6 @@ package pl.czyzniek.openfdareader.drugrecordapplication.fda;
 import java.util.List;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,5 +11,5 @@ public interface DrugRecordApplicationControllerDefinition {
     @GetMapping("/drug-record-applications/fda")
     List<DrugRecordApplication> findFdaDrugRecordApplications(@RequestParam(required = false) String manufacturerName,
                                                               @RequestParam(required = false) String brandName,
-                                                              @ParameterObject @RequestParam(required = false) @PageableDefault Pageable pageable);
+                                                              @ParameterObject @RequestParam(required = false) Pageable pageable);
 }
